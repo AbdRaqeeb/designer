485d3f023be89a005f3ddbab2e4b52388f89dd2f
Abdur-Raqeeb Ajao <ajaorqb@gmail.com>
2021-02-09 20:26:05 +0100

    Initial commit

---
+++ public/javascript/script.js
@@ -0,0 +1,110 @@
+// eyowo 
+const eyowo = document.querySelector('#eyowo');
+const eyoShow = document.querySelector('#eyo-first');
+const eyoHide = document.querySelector('#eyo-second');
+const eyoBall = document.querySelector('#eyo-ball');
+
+// Eyowo hover
+eyowo.addEventListener('mouseenter', function () {
+    eyoShow.classList.add('hidden');
+    eyoHide.classList.remove('hidden');
+    eyoBall.classList.remove('hidden');
+});
+
+eyowo.addEventListener('mouseleave', function () {
+    eyoShow.classList.remove('hidden');
+    eyoHide.classList.add('hidden');
+    eyoBall.classList.add('hidden');
+});
+
+
+// Navbar
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#navbar');
+
+menu.addEventListener('click', function() {
+    if(nav.classList.contains('hidden')) {
+        nav.classList.remove('hidden')
+    } else {
+        nav.classList.add('hidden');
+    }
+});
+
+
+// doktor 
+const doktor = document.querySelector('#doktor');
+const doktorShow = document.querySelector('#doktor-first');
+const doktorHide = document.querySelector('#doktor-second');
+const doktorBall = document.querySelector('#doktor-ball');
+
+// doktor hover
+doktor.addEventListener('mouseenter', function () {
+    doktorShow.classList.add('hidden');
+    doktorHide.classList.remove('hidden');
+    doktorBall.classList.remove('hidden');
+});
+
+doktor.addEventListener('mouseleave', function () {
+    doktorShow.classList.remove('hidden');
+    doktorHide.classList.add('hidden');
+    doktorBall.classList.add('hidden');
+});
+
+
+// cabin 
+const cabin = document.querySelector('#cabin');
+const cabinShow = document.querySelector('#cabin-first');
+const cabinHide = document.querySelector('#cabin-second');
+const cabinBall = document.querySelector('#cabin-ball');
+
+// cabin hover
+cabin.addEventListener('mouseenter', function () {
+    cabinShow.classList.add('hidden');
+    cabinHide.classList.remove('hidden');
+    cabinBall.classList.remove('hidden');
+});
+
+cabin.addEventListener('mouseleave', function () {
+    cabinShow.classList.remove('hidden');
+    cabinHide.classList.add('hidden');
+    cabinBall.classList.add('hidden');
+});
+
+
+// maal 
+const maal = document.querySelector('#maal');
+const maalShow = document.querySelector('#maal-first');
+const maalHide = document.querySelector('#maal-second');
+const maalBall = document.querySelector('#maal-ball');
+
+// maal hover
+maal.addEventListener('mouseenter', function () {
+    maalShow.classList.add('hidden');
+    maalHide.classList.remove('hidden');
+    maalBall.classList.remove('hidden');
+});
+
+maal.addEventListener('mouseleave', function () {
+    maalShow.classList.remove('hidden');
+    maalHide.classList.add('hidden');
+    maalBall.classList.add('hidden');
+});
+
+// kiyah 
+const kiyah = document.querySelector('#kiyah');
+const kiyahShow = document.querySelector('#kiyah-first');
+const kiyahHide = document.querySelector('#kiyah-second');
+const kiyahBall = document.querySelector('#kiyah-ball');
+
+// kiyah hover
+kiyah.addEventListener('mouseenter', function () {
+    kiyahShow.classList.add('hidden');
+    kiyahHide.classList.remove('hidden');
+    kiyahBall.classList.remove('hidden');
+});
+
+kiyah.addEventListener('mouseleave', function () {
+    kiyahShow.classList.remove('hidden');
+    kiyahHide.classList.add('hidden');
+    kiyahBall.classList.add('hidden');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+const plugin = require('tailwindcss/plugin')
+
+
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./public/index.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#121212',
+        secondary: '#1A1A1A',
+        judge: {
+          100: '#055DE5',
+          200: '#10CFCF'
+        },
+        eyo: '#C9874A',
+        touch: '#C4C4C4',
+        kiyah: '#CC131D',
+        maal: '#21995A',
+        cabin: '#4CC9F0',
+        doktor: '#07B892',
+      },
+      fontFamily: {
+        body: ['Open Sans', 'sans-serif'],
+        name: ['Fraunces', 'serif']
+      },
+      boxShadow: {
+        nav: '2px 4px 4px rgba(0, 0, 0, 0.1)',
+        card: '2px 2px 5px 5px rgba(0, 0, 0, 0.1)'
+      },
+      width: {
+        name: '504px',
+        img: '360px',
+        card: '1120px',
+        small: '426px',
+        open: '427px',
+        prod: '657px',
+        ball: '45rem'
+      },
+      height: {
+        img: '384px',
+        card: '711px',
+        small: '560px',
+        ball: '45rem',
+        down: '90vh'
+      },
+      borderRadius: {
+        card: '21px'
+      },
+      screens: {
+        xs: {'max': '320px'}
+      }
+    },
+  },
+  variants: {
+    extend: {
+      display: ['hover', 'focus']
+    },
+  },
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.filter-none': {
+          filter: 'none',
+        },
+        '.filter-blur': {
+          filter: 'blur(100px)',
+        },
+      }
+
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    })
+  ],
+}
+
